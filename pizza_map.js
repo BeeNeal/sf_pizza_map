5fe65db38b6d2fda82688e3165b71fac1843ddbf
Brittany <brittanyneal22@gmail.com>
2018-05-22 22:52:28 -0700

    added data point for marker

--- pizza_map.js
+++ pizza_map.js
@@ -11,12 +11,81 @@ map.addControl(new mapboxgl.NavigationControl());
 
 // Ajax call to local geojson
 $(document).ready(function(e){
-        $.ajax({url: "http://localhost:8000/pizza_pts.json", success: function(result){
-            var geojson = result;
+        $.getJSON({url: "http://localhost:8000/pizza_pts.json",
+            success: function(data){
+                console.log('this is working at least');
+                var geojson = data;
+                console.log(result);
         }});
 });
 
+var geojson = {
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "address": "37 Yerba Buena Lane, San Francisco, California 94103, United States",
+        "place_name": "Delarosa"
+      },
+      "geometry": {
+        "coordinates": [
+          -122.40439,
+          37.785396
+        ],
+        "type": "Point"
+      },
+      "id": "address.1047900515756646"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "address": "4042 24th Street, San Francisco, California 94114, United States",
+        "place_name": "Paxti's Pizza"
+      },
+      "geometry": {
+        "coordinates": [
+          -122.43287,
+          37.751612
+        ],
+        "type": "Point"
+      },
+      "id": "address.4490842138254994"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "address": "680 2nd Street, San Francisco, California 94107, United States",
+        "place_name": "Slice House"
+      },
+      "geometry": {
+        "coordinates": [
+          -122.39123,
+          37.780697
+        ],
+        "type": "Point"
+      },
+      "id": "address.6404482400250856"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "address": "2240 Polk St, San Francisco, CA 94109",
+        "place_name": "Gioia Pizza",
+        "region": "Russian Hill"
 
+      },
+      "geometry": {
+        "coordinates": [
+        -122.4218,
+        37.797497
+        ],
+        "type": "Point"
+      },
+      "id": "region.3425"
+    }
+  ],
+  "type": "FeatureCollection"
+};
 
 // add markers to map
 geojson.features.forEach(function(marker) {
