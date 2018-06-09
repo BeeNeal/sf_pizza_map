ab650398a9dff6215c60788806739608aedf037a
Brittany <brittanyneal22@gmail.com>
2018-06-08 23:24:39 -0700

    added batch geocoder

--- get_latlng.py
+++ get_latlng.py
@@ -5,3 +5,11 @@ def get_lat_lng(address):
 
     g = geocoder.google(address)
     return g.latlng
+
+def get_lat_lng_batched(addresses):
+    """Takes in list of addresses, returns geojson of those addresses"""
+
+    batched_coordinates = []
+    g = geocoder.google(addresses, method='batch')
+    for coordinates in g:
+        batched_coordinates.append(tuple(coordinates.address, coordinates.latlng))
